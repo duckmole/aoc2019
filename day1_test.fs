1fccfeadfef223349f11cbc5940b6d7604241442
Fabrice Nourisson <fnourisson@servebox.com>
2019-12-01 15:52:25 +0100

    Improve test suite

--- day1_test.fs
+++ day1_test.fs
@@ -1,13 +1,11 @@
 : equal
     =
     if
-        0
-    else
-        -1
+        drop 0
     endif ;
 
 : testsuite
     0
-    14 full_consumption 2 equal +
-    1969 full_consumption 966 equal +
+    -1 14 full_consumption 2 equal +
+    -2 1969 full_consumption 966 equal +
     ;
